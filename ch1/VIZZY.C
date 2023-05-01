750a91ac5327f92b2080a212124b1c2a705e90a4
vivekramsha <vivekhramsha@gmail.com>
2023-05-01 16:53:16 +0530

    ch1: Intro c language

---
+++ ch1/VIZZY.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    clrscr();
+    printf("Name\t\t:Vivek ramsha\n");
+    printf("Age\t\t:18\n");
+    printf("cours\t\t:C lauguage\n");
+    printf("Hobby\t\t:Football player\n");
+    printf("Mob.no\t\t:9873545078\n");
+    printf("School\t\t:P.R.khatiwala\n");
+    printf("Living city\t:Surat\n");
+    getch();
+}
---
+++ ch1/VIZZY1-2.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+   clrscr();
+   printf("-----\n");
+   printf("|    |\n");
+   printf("v    |\n");
+   printf("i    |\n");
+   printf("v    |\n");
+   printf("e    |\n");
+   printf("k    |\n");
+   printf("|    |\n");
+   printf("-----\n");
+   getch();
+}
